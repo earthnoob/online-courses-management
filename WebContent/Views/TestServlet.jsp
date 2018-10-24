b5d8273bc6b0b2a163f252ad5cc0c685496d3536
echoscp <lammap98@gmail.com>
2018-10-24 12:30:45 +0700

    Major structural change

---
+++ WebContent/Views/TestServlet.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Test Servlet</title>
+</head>
+<body>
+	<h1>I'm being dispatched from TestServlet.java!</h1>
+	<p>My name is: ${name}</p>
+</body>
+</html>
---
+++ src/OCM/TestServlet.java
@@ -0,0 +1,54 @@
+package OCM;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import OCM.Model.*;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO: Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<h1>This is a test servlet.</h1>"); */
+		
+		response.setContentType("text/plain");
+		
+		String str = request.getParameter("name");
+		request.setAttribute("name", str);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/Views/TestServlet.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO: Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
